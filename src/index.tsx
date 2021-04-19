9afecfffb0c69c185b1430db98ad82bcd028844f
javila35 <josepharthuravila@gmail.com>
2021-04-19 09:37:04 -0700

    Using message/eval pattern to execute code

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ const App = () => {
   const [input, setInput] = React.useState<string>("");
   const [code, setCode] = React.useState<string>("");
   const ref = React.useRef<any>();
+  const iframe = React.useRef<any>();
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -24,6 +25,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -35,8 +38,29 @@ const App = () => {
       },
     });
 
-    setCode(result.outputFiles[0].text);
+    // setCode(result.outputFiles[0].text);
+    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
   };
+
+  const html = `
+			<html>
+				<head></head>
+				<body>
+					<div id="root"></div>
+					<script>
+						window.addEventListener("message", (messageEvent) => {
+							try {
+								eval(messageEvent.data);
+							} catch (err) {
+								const root = document.querySelector("#root");
+								root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>';
+								console.error(err);
+							}
+						}, false)
+					</script>
+				</body>
+			</html>
+		`;
 
   return (
     <div>
@@ -48,6 +72,7 @@ const App = () => {
         <button onClick={onClick}>Submit</button>
       </div>
       <pre>{code}</pre>
+      <iframe sandbox="allow-scripts" srcDoc={html} ref={iframe} />
     </div>
   );
 };
