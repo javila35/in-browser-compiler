194734994b4e1dcec983f69e68bc5a694ea6fa86
javila35 <josepharthuravila@gmail.com>
2021-04-29 21:45:17 -0700

    Add FA-icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { CellList } from "./components";
 import { store } from "./state";
+import "bulmaswatch/superhero/bulmaswatch.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const App = () => {
   return (
